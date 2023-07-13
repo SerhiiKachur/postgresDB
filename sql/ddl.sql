87a79c62db90e536007c97324d248e225b848666
SerhiiKachur <kachurserega28@gmail.com>
2023-07-13 16:49:24 +0300

    add creation of sql table

---
+++ sql/ddl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users(
+  first_name VARCHAR(64),
+  last_name VARCHAR(64),
+  email VARCHAR(256),
+  height NUMERIC (3,2),
+  is_male BOOLEAN,
+  birthday DATE
+
+);
+
+DROP TABLE users;
