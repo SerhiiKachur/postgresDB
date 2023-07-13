b130a23ba1806d31ec9cf452f471984b5eb4e943
SerhiiKachur <kachurserega28@gmail.com>
2023-07-13 17:30:22 +0300

    Add NOT NULL rule example

---
+++ sql/dml.sql
@@ -0,0 +1,12 @@
+INSERT INTO users(
+    height,
+    first_name,
+    last_name,
+    birthday,
+    email
+)
+VALUES
+(0.01,'User','Userenko', '1999-01-01', 'mail@.mail.com'),
+(1.61,'','', '1999-01-02', ''),
+(1.35,'User','Userenko', '1983-05-14','mail@.mail.com'),
+(-0.5,'User','Userenko', '1999-01-01','mail13@.mail.com');
