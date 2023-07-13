3482031f455f28e9020bdae776f10dd2df93099b
SerhiiKachur <kachurserega28@gmail.com>
2023-07-13 18:34:39 +0300

    Add Constraint examples

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -3,8 +3,8 @@ CREATE TABLE users(
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(64)NOT NULL CHECK (first_name != ''),
   last_name VARCHAR(64)NOT NULL CHECK (last_name != ''),
-  email VARCHAR(256)NOT NULL UNIQUE CHECK (email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.[a-z]{2,32}$'),
-  height NUMERIC (3,2) NOT NULL CHECK (height > 0.4 AND height < 3),
+  email VARCHAR(256)NOT NULL UNIQUE CONSTRAINT "invalid email"  CHECK (email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.[a-z]{2,32}$'),
+  height NUMERIC (3,2) NOT NULL CONSTRAINT "invalid height" CHECK (height > 0.4 AND height < 3),
   is_male BOOLEAN DEFAULT true,
   birthday DATE NOT NULL CHECK (birthday>'1900-01-01' AND birthday < current_date)
 );
--- sql/dml.sql
+++ sql/dml.sql
@@ -9,4 +9,4 @@ VALUES
 (1.01,'User','Userenko', '1999-01-01', 'mail@mail.com'),
 (1.61,'User','Userenko', '1999-01-02', 'mail2@gmail.com'),
 (1.35,'User','Userenko', '1983-05-14','mail3@mail.com'),
-(1.5,'User','Userenko', '1999-01-01','mail4@mail.com');
+(2.5,'User','Userenko', '1999-01-01','mail4@mail.com');
