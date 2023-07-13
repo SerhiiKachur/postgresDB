9248f5d92e5c9a2ece08dc8b466825b4085b8410
SerhiiKachur <kachurserega28@gmail.com>
2023-07-13 17:38:50 +0300

    Add UNIQUE sql example

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
   first_name VARCHAR(64)NOT NULL,
   last_name VARCHAR(64)NOT NULL,
-  email VARCHAR(256)NOT NULL,
+  email VARCHAR(256)NOT NULL UNIQUE,
   height NUMERIC (3,2) NOT NULL,
   is_male BOOLEAN,
   birthday DATE NOT NULL
