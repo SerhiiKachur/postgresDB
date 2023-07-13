2002dee6f15c0a003cc9b66daec373547b19d83a
SerhiiKachur <kachurserega28@gmail.com>
2023-07-13 18:30:50 +0300

    Add SERIAL and uuid  examples

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,10 +1,11 @@
 CREATE TABLE users(
-  id INTEGER PRIMARY KEY,
+  -- id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  id SERIAL PRIMARY KEY,
   first_name VARCHAR(64)NOT NULL CHECK (first_name != ''),
   last_name VARCHAR(64)NOT NULL CHECK (last_name != ''),
-  email VARCHAR(256)NOT NULL UNIQUE CHECK (email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.$'),
+  email VARCHAR(256)NOT NULL UNIQUE CHECK (email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.[a-z]{2,32}$'),
   height NUMERIC (3,2) NOT NULL CHECK (height > 0.4 AND height < 3),
-  is_male BOOLEAN,
+  is_male BOOLEAN DEFAULT true,
   birthday DATE NOT NULL CHECK (birthday>'1900-01-01' AND birthday < current_date)
 );
 
--- sql/dml.sql
+++ sql/dml.sql
@@ -1,12 +1,12 @@
 INSERT INTO users(
-    height,
-    first_name,
-    last_name,
-    birthday,
-    email
+  height,
+  first_name,
+  last_name,
+  birthday,
+  email
 )
 VALUES
-(0.01,'User','Userenko', '1999-01-01', 'mail@.mail.com'),
-(1.61,'','', '1999-01-02', ''),
-(1.35,'User','Userenko', '1983-05-14','mail@.mail.com'),
-(-0.5,'User','Userenko', '1999-01-01','mail13@.mail.com');
+(1.01,'User','Userenko', '1999-01-01', 'mail@mail.com'),
+(1.61,'User','Userenko', '1999-01-02', 'mail2@gmail.com'),
+(1.35,'User','Userenko', '1983-05-14','mail3@mail.com'),
+(1.5,'User','Userenko', '1999-01-01','mail4@mail.com');
