0919976e96327a60b53dda0722855f415bc38141
SerhiiKachur <kachurserega28@gmail.com>
2023-07-14 12:41:32 +0300

    Add task-users practice

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -10,3 +10,17 @@ CREATE TABLE users(
 );
 
 DROP TABLE users;
+
+CREATE TABLE task_users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(64)NOT NULL,
+  last_name VARCHAR(64)NOT NULL,
+  email VARCHAR(256)NOT NULL UNIQUE CONSTRAINT "invalid email"  CHECK (email ~ '^[a-zA-Z0-9]{1,32}@[a-z]{2,32}\.[a-z]{2,32}$'),
+  height NUMERIC (3,2) NOT NULL CONSTRAINT "invalid height" CHECK (height > 0.4 AND height < 3),
+  is_male BOOLEAN DEFAULT true,
+  birthday DATE NOT NULL CHECK (birthday >'1900-01-01' AND birthday < current_date),
+  weight NUMERIC (5,2) CONSTRAINT "weight must be 0 - 500kg" CHECK (weight > 0 AND weight < 500),
+  raiting NUMERIC (2,1) CONSTRAINT "raiting must be 1.0-5.0" CHECK (raiting >= 1 AND raiting <= 5),
+  CONSTRAINT "Unique full name" UNIQUE (first_name,last_name),
+  CONSTRAINT "User must have correct name" CHECK (first_name != '' AND last_name != '')
+)
--- sql/dml.sql
+++ sql/dml.sql
@@ -1,12 +1,15 @@
-INSERT INTO users(
+INSERT INTO task_users(
   height,
   first_name,
   last_name,
   birthday,
-  email
+  email,
+  is_male,
+  weight,
+  raiting
 )
 VALUES
-(1.01,'User','Userenko', '1999-01-01', 'mail@mail.com'),
-(1.61,'User','Userenko', '1999-01-02', 'mail2@gmail.com'),
-(1.35,'User','Userenko', '1983-05-14','mail3@mail.com'),
-(2.5,'User','Userenko', '1999-01-01','mail4@mail.com');
+(1.01,'User1','Userenko', '1999-01-01', 'mail@mail.com',true,123,2.5),
+(1.61,'User2','Userenko', '1999-01-02', 'mail2@gmail.com',true,233,1.0),
+(1.35,'User3','Userenko', '1983-05-14','mail3@mail.com',false,122,5.0),
+(2.5,'User4','Userenko', '1999-01-01','mail4@mail.com',true,185,1.4);
