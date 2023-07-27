cabc873c08425be98ec7fcefc59dd1e5e6ecc0bd
SerhiiKachur <kachurserega28@gmail.com>
2023-07-27 12:34:53 +0300

    Add aggregation with where filter

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -16,3 +16,5 @@ SELECT avg(height) FROM users;
 SELECT avg(height) AS "seredniy zrist", is_male FROM users GROUP BY is_male;
 SELECT min(foot_size) AS "min-foot-size", is_male FROM users GROUP BY is_male;
 SELECT min(weight),max(weight),avg(weight) AS "weight-info", is_male FROM users GROUP BY is_male;
+
+SELECT avg(height),is_male FROM users WHERE weight < 100 GROUP BY is_male;
