f0cc741b5cf108780efd206f8a4a2e27e805efa1
SerhiiKachur <kachurserega28@gmail.com>
2023-07-27 12:47:02 +0300

    Add group filtration with having

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -18,3 +18,15 @@ SELECT min(foot_size) AS "min-foot-size", is_male FROM users GROUP BY is_male;
 SELECT min(weight),max(weight),avg(weight) AS "weight-info", is_male FROM users GROUP BY is_male;
 
 SELECT avg(height),is_male FROM users WHERE weight < 100 GROUP BY is_male;
+
+SELECT * FROM (
+  SELECT count(*) amount, hair_color
+  FROM users
+  GROUP BY hair_color
+) hc_amount
+WHERE amount > 30;
+
+SELECT count(*) , hair_color 
+FROM users 
+GROUP BY hair_color
+HAVING count(*) > 30;
