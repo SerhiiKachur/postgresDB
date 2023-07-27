125cdbd15bb5cadae79760bf74677d3535ea867c
SerhiiKachur <kachurserega28@gmail.com>
2023-07-27 13:09:25 +0300

    Add sorting task

--- sql/dql.sql
+++ sql/dql.sql
@@ -17,3 +17,10 @@ SELECT * FROM (SELECT id, email, concat(first_name, ' ', last_name) AS "full_nam
 FROM users) AS "user_full_name" WHERE length(full_name) < 16;
 
 SELECT * FROM users ORDER BY height DESC, foot_size ASC;
+
+
+-- SELECT * FROM users order by weight ASC, birthday DESC, foot_size DESC;
+
+SELECT id, weight, birthday, foot_size
+FROM users
+ORDER BY weight ASC, birthday DESC, foot_size DESC;
