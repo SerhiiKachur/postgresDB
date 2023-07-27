c7ec23daff8a2ec6a9c029e5a918f2ac981956cc
SerhiiKachur <kachurserega28@gmail.com>
2023-07-27 12:16:15 +0300

    Basic task challange

--- sql/aggregation.sql
+++ sql/aggregation.sql
@@ -11,3 +11,8 @@ SELECT foot_size FROM users GROUP BY foot_size;
 SELECT count(*) AS "кількість", is_male FROM users GROUP BY is_male;
 
 SELECT count(*) AS "foot-size-count", foot_size FROM users GROUP BY foot_size;
+
+SELECT avg(height) FROM users;
+SELECT avg(height) AS "seredniy zrist", is_male FROM users GROUP BY is_male;
+SELECT min(foot_size) AS "min-foot-size", is_male FROM users GROUP BY is_male;
+SELECT min(weight),max(weight),avg(weight) AS "weight-info", is_male FROM users GROUP BY is_male;
