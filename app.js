13eba38884045084451736123c80e16a28f6114b
SerhiiKachur <kachurserega28@gmail.com>
2023-08-02 14:54:44 +0300

    add route example

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 const express = require("express");
-const UserController = require('./controllers/userController');
+const UserController = require("./controllers/userController");
 
 const app = express();
 
 const bodyParser = express.json();
 
-app.get("/", (req, res) => {
-  res.send("test");
-});
+// app.get("/users", UserController.getUsers);
 
-app.post(
-  "/users",
-  bodyParser,
-  (req, res, next) => {
-    req.test = 212324;
-    next();
-  },
-  (req, res) => {
-    console.log(req.test);
-    console.log(req.body);
-    res.send(req.body);
-  },
-  UserController.createUser
-);
+// app.post(
+//   "/users",
+//   bodyParser,
+//   (req, res, next) => {
+//     req.test = 212324;
+//     next();
+//   },
+//   (req, res) => {
+//     console.log(req.test);
+//     console.log(req.body);
+//     res.send(req.body);
+//   },
+//   UserController.createUser
+// );
+
+app
+  .route("/users")
+  .get(UserController.getUsers)
+  .post(bodyParser, UserController.createUser);
+
+app.delete("/users/:userId", UserController.deleteUser);
 
 module.exports = app;
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,4 +8,10 @@ module.exports.getUser = (req, res) => {};
 
 module.exports.updateUser = (req, res) => {};
 
-module.exports.deleteUser = (req, res) => {};
+module.exports.deleteUser = (req, res) => {
+  const {
+    params: { userID },
+  } = req;
+  
+  res.send(`user ${userID} is deleted`);
+};
