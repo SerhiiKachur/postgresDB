21eb49862da0b4f264396ee7921df06e95cc30f8
SerhiiKachur <kachurserega28@gmail.com>
2023-08-06 14:36:10 +0300

    Add POST user example

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,11 @@
-module.exports.createUser = (req, res) => {
-  res.send(req.body);
+const { User } = require("../models");
+
+module.exports.createUser = async (req, res) => {
+  const { body } = req;
+
+  const user = await User.create(body);
+
+  res.send({ data: user });
 };
 
 module.exports.getUsers = (req, res) => {};
@@ -12,6 +18,6 @@ module.exports.deleteUser = (req, res) => {
   const {
     params: { userID },
   } = req;
-  
+
   res.send(`user ${userID} is deleted`);
 };
