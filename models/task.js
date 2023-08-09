eb4ae8e24e4d6d410bdab3ddfdfcb1958b70b411
SerhiiKachur <kachurserega28@gmail.com>
2023-08-09 20:03:34 +0300

    Add 1:m association

--- models/task.js
+++ models/task.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Task.belongsTo(models.User, {
+        foreignKey:'userId'
+      });
     }
   }
   Task.init(
--- models/user.js
+++ models/user.js
@@ -8,8 +8,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate(Task) {
       // define association here
+      User.hasMany(Task,{
+        foreignKey: 'userId'
+      })
     }
   }
   User.init(
