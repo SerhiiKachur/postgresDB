1c78a2005294f8571fcc48be05031119edc8c2ee
SerhiiKachur <kachurserega28@gmail.com>
2023-08-14 17:54:02 +0300

    Add left join example(with inner join,right join  explanation)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const { Op } = require("sequelize");
-const { User } = require("../models");
+const { User,Group } = require("../models");
 const createHttpError = require("http-errors");
 
 module.exports.createUser = async (req, res, next) => {
@@ -16,7 +16,18 @@ module.exports.createUser = async (req, res, next) => {
 
 module.exports.getUsers = async (req, res, next) => {
   try {
-    const users = await User.findAll();
+    const users = await User.findAll({
+      include: [
+        {
+          model: Group,
+          through: {
+            attributes: [],
+          },
+          //required: true - INNER JOIN
+          //required: right - RIGHT JOIN
+        },
+      ],
+    });
     // const users = await User.findAll({
     //   attributes: [['first_name','name'], 'email', 'isMale'],
     // });
